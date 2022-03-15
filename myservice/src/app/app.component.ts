4c7ea59e3695e30837429d8b8f31365749e27014
Shivam <shivam04rathi@gmail.com>
2022-03-14 20:56:46 -0700

    2nd commit

---
+++ myservice/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MyserviceService } from './myservice.service';
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+   title = 'Angular 4 Project!';
+   todaydate;
+   constructor(private myservice: MyserviceService) {}
+   ngOnInit() {
+      this.todaydate = this.myservice.showTodayDate();
+   }
+}
---
+++ services-blog/src/app/customer.ts
@@ -0,0 +1,9 @@
+export class Customer {
+  id: number;
+
+   name: string;
+
+   username: string;
+
+   email: string;
+}
